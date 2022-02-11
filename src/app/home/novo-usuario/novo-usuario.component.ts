2fba759040a7b191fe3378cf9be99b9aedd13832
Vitor Machado <machado@pop-os.localdomain>
2022-02-11 15:44:37 -0300

    Validando campos do formulário via rxjs

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { minusculoValidator } from './minusculo.validator';
 import { NovoUsuario } from './novo-usuario';
 import { NovoUsuarioService } from './novo-usuario.service';
+import { UsuarioExisteService } from './usuario-existe.service';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -12,13 +14,19 @@ export class NovoUsuarioComponent implements OnInit {
 
   novoUsuarioForm!: FormGroup;
 
-  constructor(private formBuilder:FormBuilder, private novoUsuarioService: NovoUsuarioService) { }
+  constructor(
+    private formBuilder:FormBuilder,
+    private novoUsuarioService: NovoUsuarioService,
+    private usuarioExistenteService: UsuarioExisteService
+    ) { }
 
   ngOnInit(): void {
       this.novoUsuarioForm = this.formBuilder.group({
           email: ['', [Validators.required, Validators.email]],
           fullName: ['', [Validators.required, Validators.minLength(4)]],
-          userName: [''],
+          userName: ['',
+                      [minusculoValidator],
+                      [this.usuarioExistenteService.usuarioJaExiste()]],
           password: [''],
       });
   }
