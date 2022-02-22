1e42c9bb070a13e50c29e44a58f16accaee514fe
Vitor Machado <machado@pop-os.localdomain>
2022-02-21 21:54:44 -0300

    Adicionando página com a lista de fotos dos animais

--- src/app/animais/lista-animais/lista-animais.component.ts
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,4 +1,7 @@
+import { UsuarioService } from './../../autenticacao/usuario/usuario.service';
+import { Animais } from '../animais';
 import { Component, OnInit } from '@angular/core';
+import { AnimaisService } from '../animais.service';
 
 @Component({
   selector: 'app-lista-animais',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-animais.component.css']
 })
 export class ListaAnimaisComponent implements OnInit {
+  animais!: Animais;
 
-  constructor() { }
+  constructor(
+      private usuarioService: UsuarioService,
+      private animaisService: AnimaisService
+  ) { }
 
   ngOnInit(): void {
+    this.usuarioService.retornaUsuario().subscribe((usuario) => {
+         const userName = usuario.name ?? '';
+         this.animaisService.listaDoUsuario(userName).subscribe((animais) => {
+            this.animais = animais;
+         });
+    });
+
   }
 
 }
