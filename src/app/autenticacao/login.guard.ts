05e1592920a6cf616013439eea377e831d7a6b04
Vitor Machado <machado@pop-os.localdomain>
2022-02-22 09:08:18 -0300

    Adicionando Guardas de rotas

--- src/app/autenticacao/login.guard.ts
+++ src/app/autenticacao/login.guard.ts
@@ -1,23 +1,19 @@
+import { UsuarioService } from './usuario/usuario.service';
 import { Injectable } from '@angular/core';
 import { CanLoad, Route, Router, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import { UsuarioService } from './usuario/usuario.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AutenticacaoGuard implements CanLoad {
+export class LoginGuard implements CanLoad {
 
-  constructor(
-    private usuarioService: UsuarioService,
-    private router: Router
-  ) { }
-
+  constructor(private usuarioService :UsuarioService, private router: Router) { }
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if(!this.usuarioService.estaLogado()){
-      this.router.navigate([''])
+    if(this.usuarioService.estaLogado()){
+      this.router.navigate(['animais']);
       return false;
     }
     return true;
